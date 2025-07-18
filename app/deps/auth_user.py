05b616ef2e77f978f122460462de8c628da5e0ae
gauravproject76 <kumargaurav.project@gmail.com>
2025-07-18 10:44:26 +0530

    Add files via upload

---
+++ app/deps/auth_user.py
@@ -0,0 +1,22 @@
+from fastapi import Header, HTTPException
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+# Load env variables
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
+client = MongoClient(MONGO_URI)
+db = client.get_default_database()
+user_collection = db.devices
+
+async def verify_api_key_user(x_api_key: str = Header(None)):
+    if not x_api_key:
+        raise HTTPException(status_code=403, detail="Forbidden on Home")
+
+    user = user_collection.find_one({"deviceId": x_api_key, "status": "active", "role": "user"})
+    if not user:
+        raise HTTPException(status_code=403, detail="Forbidden, Not using orignal app.")
+
+    return user
